cade583e0ca45a374c037f63b0ff669c84614b01
mord4 <fedorsmolin2006@gmail.com>
2026-05-08 17:44:31 +0300

    чумавой дизайн 365

--- backend/webui/urls.py
+++ backend/webui/urls.py
@@ -1,8 +1,39 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
     path('', views.home, name='home'),
     path('files/', views.files_page, name='files_page'),
     path('equipment/', views.equipment_page, name='equipment_page'),
+
+    path(
+        'files/<int:file_id>/update-prompt/',
+        views.update_prompt,
+        name='update_prompt',
+    ),
+
+    path(
+        'applications/',
+        views.applications_page,
+        name='applications_page',
+    ),
+
+    path(
+        'applications/<int:application_id>/',
+        views.application_detail_page,
+        name='application_detail_page',
+    ),
+
+    path(
+        'applications/<int:application_id>/select-shop/',
+        views.select_shop,
+        name='select_shop',
+    ),
+
+    path(
+        'applications/<int:application_id>/contract/',
+        views.contract_page,
+        name='contract_page',
+    ),
 ]
